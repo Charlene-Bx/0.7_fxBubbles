63fa93ef4ce7df393d39e2a2ae5ce3375b83d19c
Charlene-Bx <charlene.bonnardeaux@gmail.com>
2021-03-18 13:47:54 +0100

    some animations and scss added

--- controllers/router.js
+++ controllers/router.js
@@ -12,36 +12,37 @@ exports.router =(()=>{
     router.route('/')                               //Définis une route, ici, pour le chemin root
         .get((req, res) => {
             res.render('index',{                    //reponse > afficher la page index
+                pageActive: 'home'
                 // user: req.session.user              //Envoies les données sous forme de cookies
             });                    
         });
 
     // Training ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
     router.route('/training')                        
-        .get((req,res)=>{res.render('./pages/training')})
+        .get((req,res)=>{res.render('./pages/training',{pageActive: 'training'})})
 
     router.route('/tutorials')                        
-        .get((req,res)=>{res.render('./pages/tutorials')})
+        .get((req,res)=>{res.render('./pages/tutorials',{pageActive: 'tutorials'})})
     
     router.route('/agenda')                        
         .get((req,res)=>{
             Event.find({},(e,events)=>{
-                res.render('./pages/agenda',{events:events})
-            })
-            
+                res.render('./pages/agenda',{pageActive: 'agenda', events:events})
+            }).sort({date:1})
+
         })
 
     router.route('/infos')                        
-        .get((req,res)=>{res.render('./pages/infos')})
+        .get((req,res)=>{res.render('./pages/infos',{pageActive: 'infos'})})
 
     router.route('/account')                        
-        .get((req,res)=>{res.render('./pages/account')})
+        .get((req,res)=>{res.render('./pages/account',{pageActive: 'account'})})
 
     router.route('/signin')                        
-        .get((req,res)=>{res.render('./pages/signin')})
+        .get((req,res)=>{res.render('./pages/signin',{pageActive: 'signin'})})
 
     router.route('/signup')                        
-        .get((req,res)=>{res.render('./pages/signup')})
+        .get((req,res)=>{res.render('./pages/signup',{pageActive: 'signup'})})
 
     return router
 })();
--- public/asset/js/admin_event.js
+++ public/asset/js/admin_event.js
@@ -1,18 +1,76 @@
 let isFolded=true;
 
 const seeEvent = (target) => {
+    
+
     const card = target.parentElement.parentElement.parentElement;
     const card_height = card.scrollHeight;
 
     const subbody = card.lastElementChild;
-    const subbody_height = subbody.scrollHeight;
+
+    let subbody_height;
+
     if(isFolded){
         isFolded=false;
         subbody.style.visibility='visible';
+        subbody.children[1].style.display='none';
+        subbody.children[0].style.display='initial';
+        subbody_height = subbody.scrollHeight;
         card.style.height= `${card_height+subbody_height}px`;
     }else{
         isFolded=true;
         subbody.style.visibility='hidden';
+        subbody.children[0].style.display='none';
         card.style.height= `15rem`;
     }
 }
+
+const editEvent = (target) => {
+
+    const card = target.parentElement.parentElement.parentElement;
+    const card_height = card.scrollHeight;
+
+    const subbody = card.lastElementChild;
+
+    let subbody_height;
+
+    if(isFolded){
+        isFolded=false;
+        subbody.style.visibility='visible';
+        subbody.children[0].style.display='none';
+        subbody.children[1].style.display='initial';
+        subbody_height = subbody.scrollHeight;
+        card.style.height= `${card_height+subbody_height}px`;
+    }else{
+        isFolded=true;
+        subbody.style.visibility='hidden';
+        subbody.children[0].style.display='none';
+        card.style.height= `15rem`;
+    }
+}
+
+//Changer la couleur du tag en fonction de la categorie
+const categories= document.querySelectorAll('.event-category');
+
+const color_festival = '#E0A66F';
+const color_workshop = '#94463A';
+const colors_conference = '#046666';
+const colors_concours = '#3A946B';
+
+categories.forEach(category=>{
+
+    switch(category.textContent){
+        case 'festival':
+            category.style.backgroundColor= color_festival;
+            break;
+        case 'workshop':
+            category.style.backgroundColor= color_workshop;
+            break;
+        case 'conférence':
+            category.style.backgroundColor= colors_conference;
+            break;
+        case 'concours':
+            category.style.backgroundColor= colors_concours;
+            break;
+    }
+})
